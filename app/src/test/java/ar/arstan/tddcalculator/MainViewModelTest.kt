fa3a2cc7e760e46c9b38efa1c67ccd97e7a8f8a6
arstanrakhmat <arstanbek.rakh@gmail.com>
2025-11-14 22:41:32 +0600

    Step 3: Corner case MainViewModelTest.kt

--- app/src/test/java/ar/arstan/tddcalculator/MainViewModelTest.kt
+++ app/src/test/java/ar/arstan/tddcalculator/MainViewModelTest.kt
@@ -35,4 +35,40 @@ class MainViewModelTest {
         assertEquals("1+2", inputFlow.value)
         assertEquals("3", resultFlow.value)
     }
+
+    @Test
+    fun sum_of_two_numbers_corner_case() {
+        val inputFlow: StateFlow<String> = viewModel.inputFlow
+        val resultFlow: StateFlow<String> = viewModel.resultFlow
+
+        assertEquals("", inputFlow.value)
+        assertEquals("", resultFlow.value)
+
+        viewModel.inputOne()
+        assertEquals("1", inputFlow.value)
+
+        var expected = "1"
+        repeat(9) {
+            viewModel.inputZero()
+            expected += 0
+            assertEquals(expected, inputFlow.value)
+        }
+
+        viewModel.plus()
+        assertEquals("1000000000+", inputFlow.value)
+
+        viewModel.inputTwo()
+        assertEquals("1000000000+2", inputFlow.value)
+
+        expected = "1000000000+2"
+        repeat(9) {
+            viewModel.inputZero()
+            expected += 0
+            assertEquals(expected, inputFlow.value)
+        }
+
+        viewModel.calculate()
+        assertEquals("1000000000+2000000000", inputFlow.value)
+        assertEquals("3000000000", resultFlow.value)
+    }
 }
