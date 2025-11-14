de7f2fa31d00d0dc29addd649b735a719d9f0f6a
arstanrakhmat <arstanbek.rakh@gmail.com>
2025-11-14 18:33:09 +0600

    Preparation to step 1

---
+++ app/src/androidTest/java/ar/arstan/tddcalculator/ScenarioUiTest.kt
@@ -0,0 +1,19 @@
+package ar.arstan.tddcalculator
+
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class ScenarioUiTest {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+    @Test
+    fun scenario_number_one() {
+
+    }
+}
---
+++ app/src/test/java/ar/arstan/tddcalculator/MainViewModelTest.kt
@@ -0,0 +1,11 @@
+package ar.arstan.tddcalculator
+
+import org.junit.Test
+
+class MainViewModelTest {
+
+    @Test
+    fun scenario_number_one() {
+
+    }
+}
